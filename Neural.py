a36bf982a9e270bb778cd1f595e95dca2a89d437
nicegodcomplex <bendpetch@gmail.com>
2018-05-13 21:17:48 +0800

    Added dot product to netMath and started calc()

--- Neural.py
+++ Neural.py
@@ -1,6 +1,5 @@
 import netMath
 import random
-#import numpy
 
 class Neural(object):
     def __init__(self, numInputs, numOutputs, numLayers, numNodes):
@@ -30,17 +29,18 @@ class Neural(object):
 
     def randomiseWeights(self):
         self.__weights = [[]]*(self.__numLayers+1)
-        #self.__weights is formatted as [left layer][leftNodeIndex][rightNodeIndex]
-        for layer in range(self.__numLayers+1):
-            numLeftNodes = self.__numInputs if layer == 0 else self.__numNodes
-            numRightNodes = self.__numOutputs if layer == self.__numLayers else self.__numNodes
-            self.__weights[layer] = [[[]]*numRightNodes]*numLeftNodes
-            for l in range(numLeftNodes):
-                for r in range(numRightNodes):
-                    self.__weights[layer][l][r] = random.randrange(-5,5)
+        #self.__weights is formatted as [right layer][rightNodeIndex][leftNodeIndex]
+        for layer in range(1,self.__numLayers+2):
+            numLeftNodes = self.__numInputs if layer == 1 else self.__numNodes
+            numRightNodes = self.__numOutputs if layer == self.__numLayers+1 else self.__numNodes
+            self.__weights[layer] = [[[]]*numLeftNodes]*numRightNodes
+            for r in range(numRightNodes):
+                for l in range(numLeftNodes):
+                    self.__weights[layer][r][l] = random.randrange(-5,5)
 
     def randomiseBiases(self):
         self.__biases = [[]]*(self.__numLayers+2)
+        #self.__biases is formatted [layer][nodeIndex]
         for layer in range(self.__numLayers+2):
             numNodes = None
             if layer == 0:
@@ -50,7 +50,19 @@ class Neural(object):
             else:
                 numNodes = self.__numNodes
             self.__biases[layer] = [random.randrange(-5,5) for i in range(numNodes)]
+
+    def getNumNodesForLayer(layer):
+        if layer == 0:
+            return self.__numInputs
+        elif layer == self.__numLayers+1:
+            return self.__numOutputs
+        else:
+            return self.__numNodes
+
+    def calc(self,inputMatrix):
+        assert(len(inputMatrix)==self.__numInputs)
         
+    
 if __name__ == "__main__":
     n = Neural(2,12,3,8)
                 
--- netMath.py
+++ netMath.py
@@ -3,3 +3,14 @@ import math
 def sigmoid(x):
     return 1/(1+(math.e**(-x)))
 
+#takes two 1D arrays a and b and returns the dot product
+def dot(a,b):
+    assert(len(a)==len(b))
+    return sum([a[x]*b[x] for x in range(len(a))])
+
+if __name__ == "__main__":
+    #unit testing / misc
+    a = [4,6,3,1]
+    b = [2,1,4,5]
+    assert(dot(a,b) == 8+6+12+5)
+    assert(dot([1,0],[0,1])==0)
