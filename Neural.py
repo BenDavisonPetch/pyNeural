c40c20ad4da0f262a9645b6bff89967a4872ca41
nicegodcomplex <bendpetch@gmail.com>
2018-05-15 20:58:40 +0800

    Added calculation for cost gradient of activations and weights, and added some derivation of the maths in the readme

--- Neural.py
+++ Neural.py
@@ -1,5 +1,6 @@
 import netMath
 import random
+import copy
 
 class Neural(object):
     def __init__(self, numInputs, numOutputs, numLayers, numNodes, outputMap):
@@ -14,6 +15,13 @@ class Neural(object):
         assert(self.__numInputs == len(outputMap))
         self.__outputMap = outputMap
 
+    def emptyNodeList(self):
+        nlist = [[]]*self.__numLayers+2
+        for layer in range(self.__numLayers+2):
+            numNodes = getNumNodesForLayer(layer)
+            nlist[layer] = [0]*numNodes
+        return nlist
+    
     def initNodes(self):
         self.__nodes = []
         #self.__nodes is structured as [layer][number (from top if visualising)]
@@ -63,6 +71,7 @@ class Neural(object):
         else:
             return self.__numNodes
 
+    #goes through and calculates all the values of the neurons without doing any backprop
     def calc(self,inputMatrix):
         assert(len(inputMatrix)==self.__numInputs)
         #set input nodes
@@ -86,10 +95,54 @@ class Neural(object):
                 currentMax = output[i]
         assert(maxIndex != -1)
         return self.__outputMap[maxIndex]
+
+    #takes in desired output and an input, and returns gradient
+    #in form (weightmatrix,biasmatrix) 
+    def backprop(self, inputMatrix, desiredOutput):
+        assert(len(inputMatrix)==self.__numInputs)
+        assert(len(desiredOutput)==self.__numOutputs)
+        #set input nodes
+        self.__nodes[0] = inputMatrix
+
+        #calculate activation for each node
+        for layer in range(1,self.__numLayers+2):
+            numNodes = self.getNumNodesForLayer(layer)
+            for node in range(numNodes):
+                z = netMath.dot(self.__nodes[layer-1],self.__weights[layer][node])+self.__biases[layer][node]
+                self.__nodes[layer][node] = netMath.sigmoid(z)
+                
+        #calculate dC/da for all nodes
+        dC = self.__emptyNodeList()
+        #calculate dC/da for output nodes
+        for node in range(self.__numOutputs):
+            outL = self.__numNodes+1
+            dC[outL][node] = 2*(self.__nodes[outL][node]-desiredOut[node])
+
+        #calculate dC/da for all other nodes
+        for layer in range(self.__numLayers,0,-1):
+            for node in range(self.__numNodes):
+                dsum = 0
+                for j in range(self.getNumNodesForLayer(layer+1)):
+                    dz = self.__weights[layer+1][j][node]
+                    dsig = self.__nodes[layer+1][j]*(1-self.__nodes[layer+1][j])
+                    dCn = dC[layer+1][j]
+                    dsum += dz*dsig*dCn
+                dC[layer][node] = dsum
+
+        #calculate dC/dW for all weights
+        dW = self.__emptyWeightList()
+        for rlayer in range(self.__numLayers+1,0,-1):
+            for rnode in range(self.getNumNodesForLayer(rlayer)):
+                for lnode in range(self.getNumNodesForLayer(rlayer-1)):
+                    rA = self.__nodes[rlayer][rnode]
+                    lA = self.__nodes[rlayer-1][lnode]
+                    dW[rlayer][rnode][lnode] = lA*rA*(1-rA)*dC[rlayer][rnode]                   
+
         
+        assert("Finished" == False)
     
 if __name__ == "__main__":
     n = Neural(5,5,3,8,[x for x in range(5)])
-    out = n.predict([0.4,0.1,0.3,0.8,1.0])
+    out = n.backprop([0.4,0.1,0.3,0.8,1.0],[0,0,1,0,0])
     print(out)
                 
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # pyNeural (Very WIP)
 
 A collection of python 3 scripts built using only internal libraries that serves as a basic neural network.
+
+*Derivation of backpropagation calculus (Also WIP)*
+
+C = our cost function
+a[L][i] = activation of node in layer L at index i
+w[L][j][k] = weight between node[L][j] and node[L-1][k]
+
+dC/da for all nodes in the output layer = 2(a-y) where y is the intended output for that node
+
+Otherwise,
+dC/da[L][i] = sum(dz[L+1][j]/da[L][i] * da[L+1][j]/dz[L+1][j] * dC/a[L+1][j]) for j = 0 -> (number of nodes in layer L+1)-1
+            = sum(w[L+1][j][i] * a[L+1][j]*(1-a[L+1][j]) * dC/a[L+1][j]) for j ...
